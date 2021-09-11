2949c54be70bc1a01ddd422dd46aed50c27e015c
Bill Menees <Bill@Menees.com>
2021-09-11 09:40:14 -0500

    Supported ToggleFiles between .razor and .razor.cs.

--- src/Menees.VsTools/ToggleFilesHandler.cs
+++ src/Menees.VsTools/ToggleFilesHandler.cs
@@ -19,7 +19,7 @@
 		#region Private Data Members
 
 		private static readonly HashSet<Language> SupportsToggleFiles = new(
-			new[] { Language.CSharp, Language.VB, Language.CPlusPlus, Language.HTML, Language.XML, Language.XAML });
+			new[] { Language.CSharp, Language.VB, Language.CPlusPlus, Language.HTML, Language.XML, Language.XAML, Language.Razor });
 
 		// Use arrays (not HashSets) so we can search for extensions
 		// in a preferred order when opening the target file.
