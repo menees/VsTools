6714227d23632222421e5ececeb89b3fad4bfb19
Bill Menees <Bill@Menees.com>
2023-12-07 19:47:12 -0600

    Rollback VS SDK packages. Fix #7.

--- src/Menees.VsTools/MainPackage.Version.cs
+++ src/Menees.VsTools/MainPackage.Version.cs
@@ -20,7 +20,7 @@ namespace Menees.VsTools
 		//
 		// Note: When the version changes (major, minor, build, or revision), also update:
 		// - source.extension.vsixmanifest: <Identity Version="*"/>
-		internal const string Version = VersionYear + ".0.8";
+		internal const string Version = VersionYear + ".0.9";
 
 		internal const string Title = "Menees VS Tools";
 
